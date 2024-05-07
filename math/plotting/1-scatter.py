b545e7ee94c63ae374ffe653e4e0b58213ed826c
k-ganda <116561806+k-ganda@users.noreply.github.com>
2024-05-07 21:24:34 +0300

    Update 1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -8,8 +8,8 @@ np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
 
+plt.scatter(x, y, c='magenta', s=6)
 plt.title("Men's Height vs Weight")
 plt.xlabel('Height (in)')
 plt.ylabel('Weight (lbs)')
-plt.scatter(x, y, c='magenta', s=6)
 plt.show()
