f6baf91dad958a43716b77c9ac5a5b3791191c1e
k-ganda <k.ganda@alustudent>
2024-05-15 03:17:07 -0700

    multiplication

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """np_mult"""
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
@@ -13,17 +14,4 @@ def np_matmul(mat1, mat2):
     Returns:
         numpy.ndarray: The result of multiplying mat1 with mat2.
     """
-    m = len(mat1)
-    n = len(mat2[0])
-    p = len(mat2)
-
-    # Initialize the result matrix with zeros
-    result = [[0] * n for _ in range(m)]
-
-    # Perform matrix multiplication
-    for i in range(m):
-        for j in range(n):
-            for k in range(p):
-                result[i][j] += mat1[i][k] * mat2[k][j]
-
-    return result
+    return np.dot(mat1, mat2)
