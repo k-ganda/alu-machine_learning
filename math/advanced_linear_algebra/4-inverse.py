10a30dcb5bb0ae265de9f26346f40cf7b38e036e
k-ganda <k.ganda@alustudent>
2024-05-22 08:11:55 -0700

    step_1

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python3
 """
-Defines function that calculates the adjugate matrix of a matrix
+Defines function that calculates the inverse of a matrix
 """
+
+
+def inverse(matrix):
+    """
+    Calculates the inverse of a matrix
+
+    parameters:
+        matrix [list of lists]:
+            matrix whose inverse should be calculated
+
+    returns:
+        the inverse of matrix or None if matrix is singular
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a list of lists")
+    matrix_size = len(matrix)
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(row) != matrix_size or len(row) == 0:
+            raise ValueError("matrix must be a non-empty square matrix")
+    det = determinant(matrix)
+    if det == 0:
+        return None
+    adjugate_matrix = adjugate(matrix)
+    inverse_matrix = []
+    for row_idx in range(matrix_size):
+        inverse_row = []
+        for column_idx in range(matrix_size):
+            inverse_row.append(adjugate_matrix[row_idx][column_idx] / det)
+        inverse_matrix.append(inverse_row)
+    return inverse_matrix
 
 
 def adjugate(matrix):
