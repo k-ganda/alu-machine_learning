eb3f99c680d2969c0d12faf13cf2d38191fb27e4
k-ganda <k.ganda@alustudent>
2024-06-14 23:17:29 -0700

    trial

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,51 +1,74 @@
 #!/usr/bin/env python3
-"""Module numpy being imported"""
+
+'''This script calculates the Multivers dist and PDF'''
+
+
 import numpy as np
-"""Class multinormal"""
 
 
 class MultiNormal:
     """
-    class multinormal
+    Represents a Multivariate Normal distribution.
     """
+
     def __init__(self, data):
         """
-        class constructor
-        data is a numpy.ndarray of shape (d, n) containing the data set:
-        n is the number of data points
-        d is the number of dimensions in each data point
+        Class constructor.
+
+        Parameters:
+        data (numpy.ndarray): A 2D array of shape (d, n)
+            n (int): The number of data points
+            d (int): The number of dimensions in each data point
+
+        Raises:
+        TypeError: If data is not a 2D numpy.ndarray
+        ValueError: If n is less than 2
         """
-        if not isinstance(data, np.ndarray) or data.ndim != 2:
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
+
         d, n = data.shape
+
         if n < 2:
             raise ValueError("data must contain multiple data points")
 
-        self.data = data
-        mean = np.mean(data, axis=1).reshape(data.shape[0], 1)
-        self.mean = mean
-        cov = np.matmul(data - self.mean, data.T) / (data.shape[1] - 1)
-        self.cov = cov
+        # Calculate the mean of the data set
+        self.mean = np.mean(data, axis=1).reshape(d, 1)
+
+        # Center the data by subtracting the mean
+        data_centered = data - self.mean
+
+        # Calculate the covariance matrix
+        self.cov = np.dot(data_centered, data_centered.T) / (n - 1)
 
     def pdf(self, x):
         """
-        calculates the PDF at a data point
-        d - dimensions of multinomial instance
-        returns value of PDF
+        Calculates the PDF at a data point.
+
+        Parameters:
+        x (numpy.ndarray): A 2D array of shape (d, 1)
+        Returns:
+        float: The value of the PDF
+
+        Raises:
+        TypeError: If x is not a numpy.ndarray
+        ValueError: If x is not of shape (d, 1)
         """
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
-        if x.shape != (self.mean.shape[0], 1):
-            raise ValueError(
-                'x must have the shape ({:d}, 1)'.
-                format(self.mean.shape[0])
-            )
-        d = self.cov.shape[0]
-        x_m = x - self.mean
-        cov_inv = np.linalg.inv(self.cov)
-        det = np.linalg.det(self.cov)
-        pi = 3.1415926536
-        prefactor = 1.0 / (np.sqrt((2 * pi) ** d * det))
-        exponent = -0.5 * np.matmul(np.matmul(x_m.T, cov_inv), x_m)
-        pdf = prefactor * np.exp(exponent)
-        return pdf[0][0]
+
+        d = self.mean.shape[0]
+
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        # Calculate the PDF
+        det_cov = np.linalg.det(self.cov)
+        inv_cov = np.linalg.inv(self.cov)
+        norm_factor = 1 / np.sqrt((2 * np.pi) ** d * det_cov)
+        x_centered = x - self.mean
+        exponent = -0.5 * np.dot(np.dot(x_centered.T, inv_cov), x_centered)
+
+        pdf_value = norm_factor * np.exp(exponent)
+
+        return float(pdf_value)
