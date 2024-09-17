965be3947f6968abc2b124e6afc2ac388e5f008d
k-ganda <k.ganda@alustudent>
2024-09-17 04:36:26 -0700

    naming layers correctly

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-"""Importing tensorflow"""
+
+"""This module contains a function that
+returns tensor output of the layer"""
+
 import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
     """
-    prev: tensor output of previous
-    n: no of nodes in layer
-    activation: activation function
-    Returns: tensor output
+    prev - tensor output of previous layer
+    n - no. of nodes in layer to create
+    activation - activation function
+    layer - name of layers
     """
-    # He et al initializer for weights
+    # He et al. initializer for the layer weights
     initializer = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
 
-    # Creating the layer
+    # Create the layer
     layer = tf.layers.dense(inputs=prev,
                             units=n,
                             activation=activation,
