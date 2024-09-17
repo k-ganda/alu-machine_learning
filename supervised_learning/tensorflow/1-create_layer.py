fc8e3b8364dc34adf4eba954bb398a65079a9d00
k-ganda <k.ganda@alustudent>
2024-09-17 03:58:47 -0700

    creating dense layers

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Importing tensorflow"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    prev: tensor output of previous
+    n: no of nodes in layer
+    activation: activation function
+    Returns: tensor output
+    """
+    initializer = tf.contrib.layers.
+    variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(
+            inputs=prev,
+            units=n,
+            activation=activation,
+            kernel_initializer=initializer,
+            name="layer"
+            )
+    return layer
