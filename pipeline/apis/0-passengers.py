0bba4ea70d5b8d3b1f9c38a7c4fc24b64c844a18
k-ganda <k.ganda@alustudent>
2024-10-23 04:30:30 -0700

    pagination task0

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-"""Importing requests"""
+"""
+create a method that returns the list of ships
+that can hold a given number of passengers
+"""
+
 import requests
-"""
-Module containin a function
-to return list of ships
-"""
 
 
 def availableShips(passengerCount):
-    """
-    Returns list of ships that
-    can hold a given no of passengers
-    if no ship available
-    return empty list
-    """
+    """Returns a list of ships"""
     url = 'https://swapi-api.alx-tools.com/api/starships/'
-    ships = []
-    while url:
-        # Fetch data from the API
-        response = requests.get(url)
-        data = response.json()
 
-        # Loop through each ship in the current page
-        for ship in data['results']:
-            # Check if the passenger field is known and a valid number
-            passengers = ship['passengers']
-            if passengers.isdigit():  # Some may be 'unknown'
+    res = requests.get(url)
+    output = []
+
+    while res.status_code == 200:
+        res = res.json()
+        for ship in res['results']:
+            passengers = ship['passengers'].replace(',', '')
+            try:
                 if int(passengers) >= passengerCount:
-                    ships.append(ship['name'])
+                    output.append(ship['name'])
 
-        # Move to the next page (if any)
-        url = data['next']
-    return ships
+            except ValueError:
+                pass
+        try:
+            res = requests.get(res['next'])
+        except Exception:
+            break
+
+    return output
