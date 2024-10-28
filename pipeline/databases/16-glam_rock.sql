5bbb9c1278d4965f1388eaabfd14df5d4ec0e3a1
k-ganda <k.ganda@alustudent.com>
2024-10-28 15:01:04 +0000

    glam rock

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -2,14 +2,7 @@
 -- Ranked by longevity
 SELECT 
     band_name,
-    CASE 
-	        WHEN formed IS NOT NULL AND split IS NOT NULL THEN split - formed
-			        WHEN formed IS NOT NULL AND split IS NULL THEN 2020 - formed
-					        ELSE NULL
-							    END AS lifespan
-							FROM 
-							    metal_bands
-							WHERE 
-							    style = 'Glam rock' AND formed IS NOT NULL
-							ORDER BY 
-							    lifespan DESC;
+    (2020 - formed) - (CASE WHEN split IS NOT NULL THEN split - formed ELSE 0 END) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
