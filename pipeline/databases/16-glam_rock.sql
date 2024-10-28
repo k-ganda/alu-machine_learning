eec4ed8c3b0e0a07cd31f2821ba43070024eaff7
k-ganda <k.ganda@alustudent.com>
2024-10-28 14:46:26 +0000

    glam rock

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,7 +1,15 @@
 -- Lists all bands with Glam rock as main style
 -- Ranked by longevity
-SELECT band_name,
-       IFNULL(split, 2020) - formed AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT 
+    band_name,
+    CASE 
+	        WHEN formed IS NOT NULL AND split IS NOT NULL THEN split - formed
+			        WHEN formed IS NOT NULL AND split IS NULL THEN 2020 - formed
+					        ELSE NULL
+							    END AS lifespan
+							FROM 
+							    metal_bands
+							WHERE 
+							    style = 'Glam rock' AND formed IS NOT NULL
+							ORDER BY 
+							    lifespan DESC;
