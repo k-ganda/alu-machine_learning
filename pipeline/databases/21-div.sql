bc6e37d5e475efd19f9f850e9bf3ee247c76f388
k-ganda <k.ganda@alustudent.com>
2024-10-29 09:08:27 +0000

    function

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,18 @@
+-- Creates function SafeDiv that divides
+-- and returns firts by second number or returns 0 
+-- if second number is equal to 0
+DELIMETER //
+
+CREATE FUNCTION SafeDiv (
+	a INT,
+	b INT)
+RETURNS FLOAT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN a / b;
+	END IF;
+END //
+
+DELIMETER ;
