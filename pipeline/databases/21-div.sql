cdca1888e87f8be543ef86e409f040130b1828a8
k-ganda <116561806+k-ganda@users.noreply.github.com>
2024-10-29 02:17:09 -0700

    Update 21-div.sql

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,7 +1,7 @@
 -- Creates function SafeDiv that divides
 -- and returns firts by second number or returns 0 
 -- if second number is equal to 0
-DELIMETER //
+DELIMITER //
 
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 
