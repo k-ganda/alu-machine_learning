3795e634e98d7056541efcea4b65da0f4cad7ad9
k-ganda <k.ganda@alustudent.com>
2025-01-28 13:46:38 +0000

    RNN encoder, self attention, RNN decoder, positonal encoding

---
+++ supervised_learning/attention/1-self_attention.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""importing library"""
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """
+    Calculates attention based on paper
+    """
+    def __init__(self, units):
+        """
+        units-no of hidden units
+        W-dense layer, prev decoder hidden
+        U-encoder hidden states
+        V-Layer with 1 units applied to
+        tanh of sum of outputs of W and U
+        """
+        if type(units) is not int:
+            raise TypeError(
+                "units must be int representing the number of hidden units")
+        super(SelfAttention, self).__init__()
+        self.W = tf.keras.layers.Dense(units=units)
+        self.U = tf.keras.layers.Dense(units=units)
+        self.V = tf.keras.layers.Dense(units=1)
+
+    def call(self, s_prev, hidden_states):
+        """
+        s_prev: prev decoder hidden state
+        hidden_states: outputs of encoder
+        returns: context, weights
+        """
+        W = self.W(tf.expand_dims(s_prev, 1))
+        U = self.U(hidden_states)
+        V = self.V(tf.nn.tanh(W + U))
+        weights = tf.nn.softmax(V, axis=1)
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
+        return context, weights
---
+++ supervised_learning/attention/4-main.py
@@ -0,0 +1,6 @@
+mport numpy as np
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+
+PE = positional_encoding(30, 512)
+print(PE.shape)
+print(PE)
