f6cdd1a474971f4fac46d3fd1cde9c6dd159d8b4
k-ganda <116561806+k-ganda@users.noreply.github.com>
2025-01-28 17:41:26 +0300

    Update 1-self_attention.py

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""importing library"""
+"""
+class SelfAttention that inherits from
+tensorflow.keras.layers.Layer to calculate
+the attention for machine translation based on this paper:
+"""
 
 
 import tensorflow as tf
@@ -7,15 +11,14 @@ import tensorflow as tf
 
 class SelfAttention(tf.keras.layers.Layer):
     """
-    Calculates attention based on paper
+    class SelfAttention that inherits from
+    tensorflow.keras.layers.Layer to calculate
+    the attention for machine translation
     """
+
     def __init__(self, units):
         """
-        units-no of hidden units
-        W-dense layer, prev decoder hidden
-        U-encoder hidden states
-        V-Layer with 1 units applied to
-        tanh of sum of outputs of W and U
+        Class constructor
         """
         if type(units) is not int:
             raise TypeError(
@@ -26,11 +29,15 @@ class SelfAttention(tf.keras.layers.Layer):
         self.V = tf.keras.layers.Dense(units=1)
 
     def call(self, s_prev, hidden_states):
-        """
-        s_prev: prev decoder hidden state
-        hidden_states: outputs of encoder
-        returns: context, weights
-        """
+        '''
+        s_prev: a tensor of shape (batch, units) containing
+        the previous decoder hidden state
+        hidden_states: a tensor of shape (batch, input_seq_len, units)
+        containing the outputs of the decoder
+        Returns: context, weights
+            context: a tensor of shape (batch, units) that
+            contains the attention vector for each time step
+        '''
         W = self.W(tf.expand_dims(s_prev, 1))
         U = self.U(hidden_states)
         V = self.V(tf.nn.tanh(W + U))
