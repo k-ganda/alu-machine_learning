7a8223675691a00c931bfdd2cb23f246b6d37e67
k-ganda <116561806+k-ganda@users.noreply.github.com>
2025-03-18 21:23:16 +0300

    monte_carlo

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,27 +1,4 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-"""Importing libraries"""
-import numpy as np
-import gym
-
-
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
-    """
-    Performs monte carlo algorithm
-    env: environment instance
-    V: (s,) value estimate
-    policy: takes state return action
-    """
-    for episode in range(episodes):
-        state = env.reset()
-        episode_history = []
-        for step in range(max_steps):
-            action = policy(state)
-            next_state, reward, done, _ = env.step(action)
-            episode_history.append((state, reward))
-            state = next_state
-            
-=======
 """
 Module for implementing the Monte Carlo algorithm.
 """
@@ -57,20 +34,13 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
             next_state, reward, done, _ = env.step(action)
             episode.append((state, reward))
             state = next_state
->>>>>>> 7c7ba595d3c6b22b60b02266c5e2757b23a73290
             if done:
                 break
 
         G = 0
-<<<<<<< HEAD
-        for state, reward in reversed(episode_history):
-            G = gamma * G + reward
-            V[state] += alpha * (G - V[state])
-=======
         for t in reversed(range(len(episode))):
             state, reward = episode[t]
             G = gamma * G + reward
             V[state] = V[state] + alpha * (G - V[state])
 
->>>>>>> 7c7ba595d3c6b22b60b02266c5e2757b23a73290
     return V
